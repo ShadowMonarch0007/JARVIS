dcc8fb9611ec67e5cc1b660788c8d072fc7ce44e
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-06-17 23:47:40 +0530

    hero loader animation done

--- src/components/Hero/subcomponents/Generating.jsx
+++ src/components/Hero/subcomponents/Generating.jsx
@@ -8,7 +8,11 @@ const Generating = ({ className }) => {
         className || ""
       } text-base`}
     >
-      <Image className="w-5 h-5 mr-4" src={loading} alt="Loading" />
+      <Image
+        className="w-5 h-5 mr-4 animate-spin"
+        src={loading}
+        alt="Loading"
+      />
       AI is generating
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -84,6 +84,13 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin": 'spin 1s linear infinite',
+      },
+      keyframes: {
+        "spin": {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
       },
       backgroundImage: {
         "radial-gradient": "radial-gradient(var(--tw-gradient-stops))",
